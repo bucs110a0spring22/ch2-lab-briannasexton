26b688e50ebc71f63490f05a159a6085f39b8d5d
Brianna O <98538246+briannasexton@users.noreply.github.com>
2022-02-07 19:15:25 +0000

    Part A

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-
+import random
 #Part A
 weeks = 16
+print(weeks, type(weeks))
 classes = 5
+print (classes, type(classes))
 tuition = 6000
+print(tuition, type(tuition))
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
-
+print(cost_per_week, type(cost_per_week))
+classes_per_week = 2
+print(classes_per_week, type(classes_per_week))
+cost_per_class = (cost_per_week / classes_per_week)
+print("Cost per class:", cost_per_class, "Have a nice day")
+print(cost_per_class, type(cost_per_class))
 
 #Part B
